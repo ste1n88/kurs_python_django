ca523379f25232922b0aef8a0b190c0f8f5084c5
ste1n88 <ste1n88@rambler.ru>
2024-06-05 16:48:07 +0400

    Добаввил имена урлов

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -4,9 +4,9 @@ from MainApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
-    path('about', views.about),
-    path('item/<int:item_id>/', views.get_item),
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('item/<int:item_id>/', views.get_item, name='item'),
     path('items/', views.get_items),
-    path('items_list/', views.get_items_list),
+    path('items_list/', views.get_items_list, name='items-list'),
 ]
