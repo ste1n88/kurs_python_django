109a004e2f1048b89ffae071a068ee030191a7fb
ste1n88 <ste1n88@rambler.ru>
2024-06-05 17:49:11 +0400

    Добавил description, сделал миграцию, переменные поменял

--- MainApp/models.py
+++ MainApp/models.py
@@ -10,5 +10,7 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveIntegerField()
+    description = models.CharField(max_length=512, default='Описания ещё нет')
 
-
+    def __repr__(self):
+        return f'Item({self.name}, {self.brand}, {self.count}, {self.description})'
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from MainApp.models import Item
+from django.core.exceptions import ObjectDoesNotExist
+
 
 # Create your views here.
 
@@ -9,14 +12,14 @@ l_name = 'Капущак'
 m_name = 'Миронович'
 phone = '8-927-687-8060'
 email = 'ste1n88@rambler.ru'
-
-items = [
-            {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
-            {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
-            {"id": 5, "name": "Coca-cola 1 литр" ,"quantity":12},
-            {"id": 7, "name": "Картофель фри" ,"quantity":0},
-            {"id": 8, "name": "Кепка Montana" ,"quantity":124},
-        ]
+#
+#items = [
+#            {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
+#            {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
+#            {"id": 5, "name": "Coca-cola 1 литр" ,"quantity":12},
+#            {"id": 7, "name": "Картофель фри" ,"quantity":0},
+#            {"id": 8, "name": "Кепка Montana" ,"quantity":124},
+#        ]
 
 ######################## DAY 1
 
@@ -58,23 +61,42 @@ def get_item(request, item_id):
                  <a href="/items/">назад к списку товаров</a>
               '''
 
-    for item in items:
-        if item["id"] == item_id:
-            id = item["id"]
-            name = item["name"]
-            quantity = item["quantity"]
-            context = {
-                        'id': id,
-                        "name": name,
-                        'quantity': quantity
-            }
-            return render (request, "item.html", context )
+#    items = Item.objects.all()
+#    print(items)
 
-    return HttpResponse ('%s%s' % (product_404, go_back) )
+    try:
+        item = Item.objects.get(id=item_id)
+    except ObjectDoesNotExist:
+        return HttpResponse(f'Item with id={item_id} not found')
+    else:
+        context = {
+                  'id': item.id,
+                  "name": item.name,
+                  'quantity': item.count,
+                  'description': item.description,
+        }
+        return render (request, "item.html", context)
+
+#    for item in items:
+#        if item.id == item_id:
+#            id = item.id
+#            name = item.name
+#            quantity = item.count
+#            context = {
+#                        'id': id,
+#                        "name": name,
+#                        'quantity': quantity
+#            }
+#            return render (request, "item.html", context )
+
+#    return HttpResponse ('%s%s' % (product_404, go_back) )
 
 
 ## 5 WORK
 def get_items(request, ):
+
+    items = Item.objects.all()
+    print(items)
 
     name = ''
     quantity = ''
@@ -99,6 +121,8 @@ def get_items(request, ):
 ######################## DAY 2
 
 def get_items_list(request, ):
+
+    items = Item.objects.all()
 
     context = { "items" : items }
 
