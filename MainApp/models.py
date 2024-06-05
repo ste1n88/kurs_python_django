162b0d3fba07110089f1348ad11d425df918e6ce
ste1n88 <ste1n88@rambler.ru>
2024-06-05 14:25:12 +0400

    первая модель и миграция

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+#class User(models.Model):
+#    name = models.
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    count = models.PositiveIntegerField()
+
+
