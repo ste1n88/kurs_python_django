6875ff79ad743b02bd50bfa54468e5c2883e9af2
ste1n88 <ste1n88@rambler.ru>
2024-06-07 10:22:21 +0400

    Исправил всё по заданию практики

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -1,10 +1,12 @@
+from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from MainApp import views
 
 urlpatterns = [
-    path('', views.index_page),
-    path('snippets/add', views.add_snippet_page),
-    path('snippets/list', views.snippets_page),
+    path('admin/', admin.site.urls),
+    path('', views.index_page, name='home'),
+    path('snippets/add', views.add_snippet_page, name='snipadd'),
+    path('snippets/list', views.snippets_page, name='sniplist'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
